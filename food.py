1c236593b81e4ad94ef1d948745bbebe9f03c104
Abhishek Pujari <abhishek.rishikesh@proton.me>
2026-01-13 12:24:04 +0530

    Add food spawning system

---
+++ food.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+import random
+
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        self.shapesize(stretch_wid = 0.4, stretch_len = 0.4)
+        self.color('purple')
+        self.penup()
+        self.speed('fastest')
+        x = random.randint(-280, 280)
+        y = random.randint(-280, 280)
+        self.goto(x, y)
+
+
+    def refresh(self):
+        x = random.randint(-280, 280)
+        y = random.randint(-280, 280)
+        self.goto(x, y)
